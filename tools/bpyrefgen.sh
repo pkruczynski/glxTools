80c8108c8ac7816586c2ef352ea5523f539da58f
kruk <kruk@op.pl>
2021-05-02 22:27:27 +0200

    -

--- tools/bpyrefgen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-$HOME/Aplikacje/blender-2.92/blender -b -P pycharm-blender/python_api/pypredef_gen.py
-rm -rf pycharm-blender/python_api/pypredef-tmp
-mv pycharm-blender/python_api/pypredef/ 2.92
